09de0fec9ab3f70dc95a5dac7dd5a5dc7a54632c
yannunes11 <yannunes112@gmail.com>
2022-10-24 13:50:58 -0300

    feat: event submit onchenge

--- reactstudies2/src/Components/Form.jsx
+++ reactstudies2/src/Components/Form.jsx
@@ -1,19 +1,44 @@
+import { useState } from 'react';
+
 function Form () {
 
     function submitUserRegister (event) {
         event.preventDefault();
-        console.log(`User: Cadastrated`);
+        console.log(`User cadastrated: ${name} with the password ${password}`);
     }
+
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
 
     return (
         <div>
             <h1>My Registration</h1>
             <form onSubmit={submitUserRegister}>
                 <div>
-                    <input input="text" placeholder="Input your name: "></input>
+                    <label htmlFor="name">Name: </label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Input your name: " 
+                        onChange={(event) => setName(event.target.value)}>
+                    </input>
                 </div>
                 <div>
-                    <input type="submit" placeholder="Register"></input>
+                    <label htmlFor="password">Password: </label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Password: "
+                        onChange={(event) => setPassword(event.target.value)}>
+                    </input>
+                </div>
+                <div>
+                    <input 
+                        type="submit" 
+                        placeholder="Register">
+                    </input>
                 </div>
             </form>
         </div>
