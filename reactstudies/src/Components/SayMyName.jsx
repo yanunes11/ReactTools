d463783e08d53670fda79a84f7376c9cb4185b5e
yannunes11 <yannunes112@gmail.com>
2022-10-24 09:51:24 -0300

    feat: css global and local(cmp)

--- reactstudies/src/Components/SayMyName.jsx
+++ reactstudies/src/Components/SayMyName.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './SayMyName.module.css'; //import from the css local
 
 function SayMyName(props) {
     const {name, photo, age, profission} = props;
     return (
-        <div>
+        <div className={styles.nameContainer}> 
             <img src={photo} alt={name}/>
-            <h2>Name: {name}</h2>
+            <h2 className={styles.nameContent}>Name: {name}</h2>
             <p>Age: {age}</p>
             <p>Profission: {profission}</p>
         </div>
